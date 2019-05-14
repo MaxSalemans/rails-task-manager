629fbe3fcfec8c0d16a8c9496eae9e985671f5c0
maxsalemans <mhrsalemans@gmail.com>
2019-05-14 16:01:35 +0200

    it works, but its still ugly

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # show all tasks
+  get 'tasks', to: 'tasks#index', as: 'tasks'
+
+  # Create a new task get
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  post 'tasks', to: 'tasks#create'
+
+  # show 1 task
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  # edit a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
